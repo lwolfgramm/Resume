d036867bedfcb5de3839582ff193c2e619f8e1e8
lwolfgramm <lsaywolfgramm@gmail.com>
2022-03-26 21:16:30 -0600

    Added project files

---
+++ index.js
@@ -0,0 +1,12 @@
+<button onclick="popUp()">Click Me</button>
+
+
+
+function popUp() {
+    let person = prompt("Please enter your name");
+
+    let greeting = "Hi ";
+
+    alert(greeting + person + "!" + "\nThank you for reviewing my resume site.");
+
+}
